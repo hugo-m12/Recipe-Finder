e8527ff5aa370e0f3d52bf486f0a5d45ca857ded
Hugo Moreira <hugo.m@live.com.pt>
2025-05-15 19:19:50 +0100

    vite config file update

--- FE/recipe-finder/vite.config.js
+++ FE/recipe-finder/vite.config.js
@@ -2,8 +2,35 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss()],
+  plugins: [react(), tailwindcss()],
+
+  build: {
+    outDir: 'dist',
+  
+    assetsInlineLimit: 4096,
+    assetsDir: 'assets',
+    
+    minify: 'esbuild',
+    manifest: true,
+    
+    chunkSizeWarningLimit: 1024,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    },
+    
+    sourcemap: true
+  },
+
+  server: {
+    port: 5173,
+    host: 'localhost',
+    open: true,
+    https: false,
+    proxy: {}
+  }
 })
