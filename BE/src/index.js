e1e6b9abe7ea46dd66953340c30e7be9dcd022ca
Hugo Moreira <hugo.m@live.com.pt>
2025-05-15 16:57:18 +0100

    BE Port

--- BE/src/index.js
+++ BE/src/index.js
@@ -5,9 +5,7 @@ const adminRouter = require("./router/adminRouter")
 
 const app = express()
 
-const port = 3000
-
-//TODO const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
